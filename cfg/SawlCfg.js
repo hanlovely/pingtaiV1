918eb9156b3b40e68f01fe809430ce6c40721d5d
zhanglifeng <251450566@qq.com>
2019-12-26 14:21:23 +0800

    .

--- cfg/SawlCfg.js
+++ cfg/SawlCfg.js
@@ -6,65 +6,68 @@
  * @author dqm
  */
 (function () {
-    var global = this;
-    var SawlCfg = {
-        name : '增信网全局配置信息',
-        edition : 'R',                                     // D：开发环境 T：测试环境 P:预发布（演示） R：生产环境
-        zxwww : {
-            name : '增信网后台接口地址',
-            D  : 'https://test.deepsec.cn',
-            T  : 'https://test.deepsec.cn',
-            P  : 'https://demo.zengxin.com.cn',
-            R  : 'https://www.zengxin.com.cn',
-        },
-        platform : {
-            name : '网络赋强平台1.0',
-            // D  : 'https://test.deepsec.cn',
-            // T  : 'https://test.deepsec.cn',
-            // P  : 'https://demo.zengxin.com.cn',
-            R  : 'http://39.100.144.0:9060',
-        },
-        deepchain : {
-            name : '深安链API接口地址',
-            D  : 'http://deepchain.deepsec.cn:8088/api/',
-            T  : 'http://deepchain.deepsec.cn:8088/api/',
-            P  : 'http://deepchain.deepsec.cn:8088/api/',
-            R  : 'http://deepchain.deepsec.cn:8088/api/',
-        },
-        deepchain_browser : {
-            name : '深安链浏览器地址接口地址',
-            D  : 'https://deepchain.zengxin.com.cn/prod/',
-            T  : 'https://deepchain.zengxin.com.cn/prod/',
-            P  : 'http://deepchain.zengxin.com.cn/prod/',
-            R  : 'https://deepchain.zengxin.com.cn/prod/',
-        },
-        entzn : {
-            name : '区块链电子公证书系统',
-            D  : 'http://localhost:8080/api-entzn/',
-            T  : 'https://test.deepsec.cn/api-entzn/',
-            P  : 'http://demo.zengxin.com.cn/api-entzn/',
-            R  : 'https://api-entzn.zengxin.com.cn/api-entzn/',
-        },
-        ceedss_b : {
-            name : '增信存B端后台接口',
-            D  : 'http://localhost:8080/api-ceedss/',
-            T  : 'https://test.deepsec.cn/api-ceedss/',
-            P  : 'http://demo.zengxin.com.cn/api-ceedss/',
-            R  : 'https://www.zengxin.com.cn/api-ceedss/',
-        },
-        ceedss_n : {
-            name : '增信存N端后台接口',
-            D  : 'http://localhost:8080/bjzx_ceedssms/',
-            T  : 'https://test.deepsec.cn/bjzx_ceedssms/',
-            P  : 'http://demo.zengxin.com.cn/bjzx_ceedssms/',
-            R  : 'https://www.zengxin.com.cn/bjzx_ceedssms/',
-        },
-    };
+  var global = this;
+  var SawlCfg = {
+    name: '网络赋强平台1.0全局配置信息',
+    edition: 'T',                                     // D：开发环境 T：测试环境 P:预发布（演示） R：生产环境
+    zxwww: {
+      name: '增信网后台接口地址',
+      T: 'https://www.zengxin.com.cn',
+      R: 'https://www.zengxin.com.cn',
+    },
+    ghlogin: {
+      name: '工行登录接口',
+      T: 'http://fqtest.deepsec.cn:9011',
+      R: 'https://icbc.bjgzc.com:9015',
+    },
+    jhlogin: {
+      name: '建行登录接口',
+      T: 'http://fqtest.deepsec.cn:10201',
+      R: 'https://fqgzgl.bjgzc.com:8088',
+    },
+    hhlogin: {
+      name: '黄河登录接口',
+      T: 'http://39.100.230.216',
+      R: 'http://hhfq55123.deepnotary.com:55123',
+    },
+    judicialData: {
+      name: '司法辅助上链数据折线图',
+      T: 'https://test.deepsec.cn',
+      R: 'http://bjzx-fysd.zengxin.com.cn',
+    },
+    eeData: {
+      name: '存证平台上链数据折线图',
+      T: 'http://47.92.240.163',
+      R: 'http://218.247.7.106:10000',
+    },
 
-    /**
-     * expose
-     */
-    global.edition = SawlCfg.edition;
-    global.SawlCfg = SawlCfg;
+    platform: {
+      name: '网络赋强平台1.0',
+      T: 'http://39.100.144.0:9060',
+      R: 'http://39.100.157.189:9060',
+    },
+    deepchain: {
+      name: '深安链API接口地址',
+      T: 'http://deepchain.deepsec.cn:8088/api/',
+      R: 'http://deepchain.deepsec.cn:8088/api/',
+    },
+    deepchain_browser: {
+      name: '深安链浏览器地址接口地址',
+      T: 'https://deepchain.zengxin.com.cn/prod/',
+      R: 'https://deepchain.zengxin.com.cn/prod/',
+    },
+    entzn: {
+      name: '区块链电子公证书系统',
+      T: 'https://api-entzn.zengxin.com.cn/api-entzn/',
+      R: 'https://api-entzn.zengxin.com.cn/api-entzn/',
+    },
+
+  };
+
+  /**
+   * expose
+   */
+  global.edition = SawlCfg.edition;
+  global.SawlCfg = SawlCfg;
 
 })();
